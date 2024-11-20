e69a3861f33cba42a423a58fa71c5f8b2c724b55
Anantha018 <63914049+Anantha018@users.noreply.github.com>
2024-11-19 23:49:47 -0500

    Update forms.py

--- classnest/classnest_Base/forms.py
+++ classnest/classnest_Base/forms.py
@@ -16,3 +16,25 @@ class InboxForm(forms.ModelForm):
     class Meta:
         model = Inbox
         fields = ['to', 'subject', 'message']
+
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import Course
+
+class CourseForm(forms.ModelForm):
+    class Meta:
+        model = Course
+        fields = ['title', 'description', 'thumbnail']
+
+# Add this class to handle user registration
+class UserRegistrationForm(UserCreationForm):
+    USER_TYPE_CHOICES = [
+        ('student', 'Student'),
+        ('instructor', 'Instructor'),
+    ]
+    user_type = forms.ChoiceField(choices=USER_TYPE_CHOICES, label="I am a")
+
+    class Meta:
+        model = User
+        fields = ['username', 'password1', 'password2', 'user_type']
